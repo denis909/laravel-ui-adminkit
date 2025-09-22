b131c2a13713e9ae2ce8b5dd1f65b6ac7f7d8231
denis909 <denis909@mail.ru>
2025-09-22 14:44:06 +0300

    n/a

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::namespace('App\Http\Controllers')->group(function() {
+    Route::prefix('test')->controller('TestController')->group(function() {
+        Route::get('send-email', 'sendEmail');
+    });
+});
